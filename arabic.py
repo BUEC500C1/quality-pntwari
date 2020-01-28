20e515499b7bc72e5e6fc432a0ec8c5ddc8b5b83
pntwari <55207309+pntwari@users.noreply.github.com>
2020-01-27 21:41:10 -0500

    Create arabic.py

---
+++ arabic.py
@@ -0,0 +1,34 @@
+#!/home/ece-student/anaconda3/bin/python
+#chmod 755 splitargs
+# -*- coding: utf-8 -*-
+
+
+# Created by: Pat Rick Ntwari
+# Homework 1, EC500
+
+
+# Converting numbers from arabic to roman numerals
+
+# Number to be converted
+
+def arab_to_roman(given):
+    arab = (1000, 900,  500, 400, 100,  90, 50,  40, 10,  9,   5,  4,   1)
+    roma = ('M',  'CM', 'D', 'CD','C', 'XC','L','XL','X','IX','V','IV','I')
+
+    result = []
+
+    for i in range(0,len(arab)):
+      intconv = int(given / arab[i])
+      result.append(roma[i] * intconv)
+      given -= arab[i] * intconv
+    result = "".join(result)
+    return result
+
+def main():
+  given = 535
+  print(arab_to_roman(given))
+
+
+
+if __name__ == "__main__":
+  main()
