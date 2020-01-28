2068e07bef3cfe46f8e5ba37a49a3562e178246e
pntwari <55207309+pntwari@users.noreply.github.com>
2020-01-27 21:53:11 -0500

    Added arabic_test for pytest

---
+++ arabic_test.py
@@ -0,0 +1,21 @@
+#!/home/ece-student/anaconda3/bin/python
+#chmod 755 splitargs
+# -*- coding: utf-8 -*-
+
+
+# Created by: Pat Rick Ntwari
+# Homework 1, EC500
+
+
+# Converting numbers from arabic to roman numerals
+
+import pytest
+import arabic
+
+def test_answer():
+    assert arabic.arab_to_roman(1994) == "MCMXCIV"
+    assert arabic.arab_to_roman(30) == "XXX"
+    assert arabic.arab_to_roman(221) == "CCXXI"
+    assert arabic.arab_to_roman(7) == "VII"
+    assert arabic.arab_to_roman(15) == "XVI"      ## WRONG
+
